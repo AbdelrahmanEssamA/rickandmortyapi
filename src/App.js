d79270a34003252ee07e52272c7a7151059becc9
Abdo <abdelrahman.arabi@procore.com>
2024-08-24 12:04:39 +0300

    remove body color

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import Characters from "./Pages/Charachters";
 import CharacterDetails from "./Pages/CharacterDetails";
 import NavBar from "./Components/NavBar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ThemeContext } from "./Components/ThemeProvider";
 import "./app.css";
 
 function App() {
-  const { isDarkMode } = useContext(ThemeContext);
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.body.classList.add("dark-mode");
-    } else {
-      document.body.classList.remove("dark-mode");
-    }
-  }, [isDarkMode]);
-
   return (
-    <div className={isDarkMode ? "dark-mode" : undefined}>
+    <div>
       <Router>
         <NavBar />
         <Routes>
