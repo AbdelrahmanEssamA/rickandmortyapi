6aea71f42a6742289686fbbfae0702e9681d9838
Abdo <abdelrahman.arabi@procore.com>
2024-08-24 13:31:12 +0300

    added theme to local storage and handled body color in dark mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Characters from "./Pages/Charachters";
 import CharacterDetails from "./Pages/CharacterDetails";
 import NavBar from "./Components/NavBar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./app.css";
+import { ThemeContext } from "./Components/ThemeProvider";
 
 function App() {
+  //get the body and if isDarkMode is true add .dark-mode class to the body
+  const { isDarkMode } = useContext(ThemeContext);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+
   return (
     <div>
       <Router>
--- src/Components/ThemeProvider.jsx
+++ src/Components/ThemeProvider.jsx
@@ -1,13 +1,18 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const savedTheme = JSON.parse(localStorage.getItem("isDarkMode"));
+  const [isDarkMode, setIsDarkMode] = useState(savedTheme || false);
 
   const toggleTheme = () => {
     setIsDarkMode((prev) => !prev);
   };
+
+  useEffect(() => {
+    localStorage.setItem("isDarkMode", JSON.stringify(isDarkMode));
+  }, [isDarkMode]);
 
   return (
     <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
