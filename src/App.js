2aa015953475980a0c52f0599f4a832fb14ac5bc
Abdo <abdelrahman.arabi@procore.com>
2024-08-24 12:03:04 +0300

    fix styles 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
+import React, { useContext, useEffect } from "react";
 import Characters from "./Pages/Charachters";
 import CharacterDetails from "./Pages/CharacterDetails";
 import NavBar from "./Components/NavBar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ThemeProvider from "./Components/ThemeProvider";
+import { ThemeContext } from "./Components/ThemeProvider";
+import "./app.css";
+
 function App() {
+  const { isDarkMode } = useContext(ThemeContext);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+
   return (
-    <ThemeProvider>
-      <div className="App">
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route path="/characters" element={<Characters />} />
-            <Route path="/characters/:id" element={<CharacterDetails />} />
-          </Routes>
-        </Router>
-      </div>
-    </ThemeProvider>
+    <div className={isDarkMode ? "dark-mode" : undefined}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/characters" element={<Characters />} />
+          <Route path="/characters/:id" element={<CharacterDetails />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/Pages/CharacterDetails.jsx
+++ src/Pages/CharacterDetails.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { fetchCharacterByID } from "../api/charactersFetcher";
+import { ThemeContext } from "../Components/ThemeProvider";
 
 const CharacterDetails = () => {
   const { id } = useParams();
   const [character, setCharacter] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const { isDarkMode } = useContext(ThemeContext);
 
   useEffect(() => {
     const getCharacter = async () => {
@@ -33,14 +35,20 @@ const CharacterDetails = () => {
   const { name, image, status, species, gender, origin } = character;
 
   return (
-    <div className="characterDetails">
+    <div className={isDarkMode ? "darkCharacterDetails" : "characterDetails"}>
       <h1>{name}</h1>
       <div className="flexContainer">
-        <img src={image} alt={character.name} />
+        <img src={image} alt={name} />
         <div className="info">
-          <p>Species: {species}</p>
-          <p>Gender: {gender}</p>
-          <p>Origin: {origin.name}</p>
+          <p>
+            <strong>Species:</strong> {species}
+          </p>
+          <p>
+            <strong>Gender:</strong> {gender}
+          </p>
+          <p>
+            <strong>Origin:</strong> {origin.name}
+          </p>
           <div className="status">
             <p className={status}>{status}</p>
           </div>
@@ -49,4 +57,5 @@ const CharacterDetails = () => {
     </div>
   );
 };
+
 export default CharacterDetails;
